07c146dc35bbfb141855118b0f95c0e3b863d7be
Kyu <rocketword@gmail.com>
2017-04-14 18:57:19 -0400

    less cleaning up, new check server for bots func

--- Human/Utils.py
+++ Human/Utils.py
@@ -2,9 +2,17 @@
 
 
 class Args(list):
-    async def toString(self):
+    def toString(self):
         return " ".join(self)
 
 
 class StartupErr(Exception):
     pass
+
+
+def check_bot_server(server):
+    bots = 0
+    for usr in server.members:
+        if usr.bot:
+            bots += 1
+    return round((bots / server.member_count * 100), 2)
